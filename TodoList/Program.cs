c50b4593d18e057a85be529808316f530189965f
a16103758-afk <a16103758@gmail.com>
2025-10-18 03:32:38 +0300

    Lesson 2, Added a user, Task 6, 7, 8

---
+++ TodoList/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Работу выполнила Сайтамирова");
+
+Console.WriteLine("Введите вашу фамилию:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Введите ваше имя:");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Введите ваш год рождения:");
+string yearOfBirthInput = Console.ReadLine();
+
+int yearOfBirth = int.Parse(yearOfBirthInput);
+int age = 2025 - yearOfBirth;
+Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
