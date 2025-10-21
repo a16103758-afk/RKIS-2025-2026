c3f3ee1d18bf7ce7214527815ea402518722df4a
a16103758-afk <a16103758@gmail.com>
2025-10-21 19:50:08 +0300

    Lesson 3: Added array 'todos' and command 'help', Task 2, 3, 4

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,14 +1,43 @@
-﻿Console.WriteLine("Работу выполнила Сайтамирова");
+﻿namespace TodoList
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Работу выполнила Сайтамирова");
 
-Console.WriteLine("Введите вашу фамилию:");
-string lastName = Console.ReadLine();
+            Console.WriteLine("Введите вашу фамилию:");
+            string lastName = Console.ReadLine();
 
-Console.WriteLine("Введите ваше имя:");
-string firstName = Console.ReadLine();
+            Console.WriteLine("Введите ваше имя:");
+            string firstName = Console.ReadLine();
 
-Console.WriteLine("Введите ваш год рождения:");
-string yearOfBirthInput = Console.ReadLine();
+            Console.WriteLine("Введите ваш год рождения:");
+            string yearOfBirthInput = Console.ReadLine();
 
-int yearOfBirth = int.Parse(yearOfBirthInput);
-int age = 2025 - yearOfBirth;
-Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
+            int yearOfBirth = int.Parse(yearOfBirthInput);
+            int age = 2025 - yearOfBirth;
+            Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
+
+            string[] todos = new string[2];
+            int count = 0;
+
+            while (true)
+            {
+                Console.Write("Введите команду: ");
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "help":
+                        Console.WriteLine("help - список команд");
+                        Console.WriteLine("profile - данные пользователя");
+                        Console.WriteLine("add - добавить задачу");
+                        Console.WriteLine("view - показать задачи");
+                        Console.WriteLine("exit - выход");
+                        break;
+                }
+            }
+        }
+    }
+}
