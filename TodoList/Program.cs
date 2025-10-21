0f3b700f19c2be9732f81b8a41731d0d8b68d301
a16103758-afk <a16103758@gmail.com>
2025-10-21 21:50:07 +0300

    Lesson 4: Moved all commands from the main method, Task 2

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -2,6 +2,9 @@
 {
     class Program
     {
+        static string[] todos = new string[2];
+        static int count = 0;
+
         public static void Main()
         {
             Console.WriteLine("Работу выполнила Сайтамирова");
@@ -19,60 +22,81 @@
             int age = 2025 - yearOfBirth;
             Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
 
-            string[] todos = new string[2];
-            int count = 0;
-
             while (true)
             {
                 Console.Write("Введите команду: ");
                 string input = Console.ReadLine();
                 string[] parts = input.Split(' ', 2);
                 string command = parts[0];
-
+                
                 switch (command)
                 {
                     case "add":
-                        if (parts.Length > 1)
-                        {
-                            if (count >= todos.Length)
-                            {
-                                string[] newArray = new string[todos.Length * 2];
-                                for (int i = 0; i < todos.Length; i++)
-                                {
-                                    newArray[i] = todos[i];
-                                }
-                                todos = newArray;
-                            }
-
-                            todos[count] = parts[1];
-                            count++;
-                            Console.WriteLine("Задача добавлена");
-                        }
+                        AddTask(parts);
                         break;
 
                     case "view":
-                        for (int i = 0; i < count; i++)
-                        {
-                            Console.WriteLine((i + 1) + ". " + todos[i]);
-                        }
+                        ViewTasks();
                         break;
 
                     case "help":
-                        Console.WriteLine("help - список команд");
-                        Console.WriteLine("profile - данные пользователя");
-                        Console.WriteLine("add - добавить задачу");
-                        Console.WriteLine("view - показать задачи");
-                        Console.WriteLine("exit - выход");
+                        ShowHelp();
                         break;
 
                     case "profile":
-                        Console.WriteLine($"Пользователь {firstName} {lastName}, возраст – {age}");
+                        ShowProfile(firstName, lastName, age);
                         break;
 
                     case "exit":
                         return;
                 }
             }
+        }
+
+        static void ExpandArrays()
+        {
+            string[] newTodos = new string[todos.Length * 2];
+            for (int i = 0; i < todos.Length; i++)
+            {
+                newTodos[i] = todos[i];
+            }
+            todos = newTodos;
+        }
+
+        static void AddTask(string[] parts)
+        {
+            if (parts.Length > 1)
+            {
+                if (count >= todos.Length)
+                {
+                    ExpandArrays();
+                }
+                todos[count] = parts[1];
+                count++;
+                Console.WriteLine("Задача добавлена");
+            }
+        }
+
+        static void ViewTasks()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {todos[i]}");
+            }
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine("help - список команд");
+            Console.WriteLine("profile - данные пользователя");
+            Console.WriteLine("add - добавить задачу");
+            Console.WriteLine("view - показать задачи");
+            Console.WriteLine("exit - выход");
+        }
+
+        static void ShowProfile(string firstName, string lastName, int age)
+        {
+            Console.WriteLine($"Пользователь {firstName} {lastName}, возраст – {age}");
         }
     }
 }
