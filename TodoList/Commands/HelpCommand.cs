96cc9b5e0805463b8152a078690b6284335b36c4
a16103758-afk <a16103758@gmail.com>
2025-11-15 02:30:59 +0300

    Lesson 9: Update class 'HelpCommand'

--- TodoList/Commands/HelpCommand.cs
+++ TodoList/Commands/HelpCommand.cs
@@ -12,7 +12,7 @@ namespace TodoList
             Console.WriteLine("add -m - добавить задачу в многострочном режиме");
             Console.WriteLine("view -i, -s, -d, -a - показать задачи");
             Console.WriteLine("read - просмотреть полный текст задачи");
-            Console.WriteLine("done - отметить задачу выполненной");
+            Console.WriteLine("status - изменить статус задачи (notstarted, inprogress, completed, postponed, failed)");
             Console.WriteLine("delete - удалить задачу");
             Console.WriteLine("update - обновить задачу");
             Console.WriteLine("exit - выход");
